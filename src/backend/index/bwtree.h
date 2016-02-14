8bf192bb575adef105bd191830d2b2cb895487b2
Dana Van Aken <dvanaken@cs.cmu.edu>
2016-02-14 15:41:21 -0500

    Added basic bw-tree interface

--- src/backend/index/bwtree.h
+++ src/backend/index/bwtree.h
@@ -12,6 +12,11 @@
 
 #pragma once
 
+#include <unordered_map>
+#include <vector>
+
+#include "backend/common/types.h"
+
 namespace peloton {
 namespace index {
 
@@ -19,8 +24,54 @@ namespace index {
 // peloton/third_party/stx/btree.h
 template <typename KeyType, typename ValueType, class KeyComparator>
 class BWTree {
+ public:
 
-  // Add your declarations here
+  // TODO: insert function
+
+  // TODO: delete function
+
+  // TODO: update function
+
+  // TODO: search function
+
+ private:
+  class Node {
+
+    // PIDs of all children
+    std::vector<uint64_t> children_;
+
+    // Level in the bw-tree, is a leaf node if level == 0
+    uint32_t level_;
+
+    // Temporary node link used during structure modifications
+    uint64_t smo_link_;
+
+    // Max key in this node
+    KeyType max_fence_;
+
+    // Returns true if this is a leaf node
+    inline bool isLeafNode() const {
+      return (level_ == 0);
+    }
+  };
+
+  class LeafNode : public Node {
+
+    // PID of next child
+    uint64_t next_leaf_;
+
+    // PID of previous child
+    uint64_t prev_leaf_;
+
+    // Pointers to all data stored in this leaf
+    std::vector<ItemPointer> data_items_;
+  };
+
+  // PID of the root node
+  uint64_t root_;
+
+  // Maps node PIDs to memory locations
+  std::unordered_map<uint64_t, void*> map_table_;
 
 };
 
