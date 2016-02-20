663a519adb562ae4b0d8ad3ed58666f845d242fc
Lin Ma <malin1993ml@gmail.com>
2016-02-19 19:20:43 -0500

    Finish adding different page types. Move them into bwtree.h.

--- src/backend/index/bwtree.h
+++ src/backend/index/bwtree.h
@@ -16,7 +16,6 @@
 #include <vector>
 
 #include "backend/common/types.h"
-#include "delta_types.h"
 
 namespace peloton {
 namespace index {
@@ -25,7 +24,31 @@ namespace index {
 // peloton/third_party/stx/btree.h
 template <typename KeyType, typename ValueType, class KeyComparator>
 class BWTree {
+  using PID = std::uint64_t;
+  constexpr PID NullPID = std::numeric_limits<PID>::max();
+
  public:
+  enum PageType {
+    // For leaf nodes.
+    LEAF_NODE,
+    // Because we only have pointers of data items, we actually don't need the
+    // modify operation.
+    // There's no update operation in bwtree_index.h, either.
+    INSERT_DELTA,
+    // MODIFY_DELTA,
+    DELETE_DELTA,
+
+    // For inner nodes.
+    INNER_NODE,
+    SPLIT_DELTA,
+    INDEX_TERM_DELTA,
+    REMOVE_NODE_DELTA,
+    NODE_MERGE_DELTA
+
+    // It seems that INDEX_TERM_DELETE_DELTA is the same as INDEX_TERM_DELTA,
+    // so currently I don't use this type.
+    // INDEX_TERM_DELETE_DELTA
+  };
 
   // TODO: insert function
 
@@ -36,47 +59,131 @@ class BWTree {
   // TODO: search function
 
  private:
-  class Node {
+  class Page {
+   protected:
+    PageType type_;
 
-    // PIDs of all children
-    std::vector<uint64_t> children_;
+   public:
+    Page(PageType type) : type_(type) {}
 
-    // Level in the bw-tree, is a leaf node if level == 0
-    uint32_t level_;
-
-    // Temporary node link used during structure modifications
-    uint64_t smo_link_;
-
-    // Max key in this node
-    KeyType min_fence_;
-
-    // Max key in this node
-    KeyType max_fence_;
-
-    // Returns true if this is a leaf node
-    inline bool isLeafNode() const {
-      return (level_ == 0);
-    }
+    const inline PageType& GetType() const { return type_; }
   };
 
-  class LeafNode : public Node {
+  class InnerNode : public Page {
+   public:
+    // Separator keys and links for the children
+    // The key in the pair is the high_key_ of the child.
+    std::vector<std::pair<KeyType, PID> > children_;
 
+    // Node link used during structure modifications
+    PID side_link_;
+
+    // Min key in this node
+    KeyType low_key_;
+
+    // Max key in this node
+    KeyType high_key_;
+
+    InnerNode(): Page(INNER_NODE), side_link_(NullPID) {}
+  };
+
+  class LeafNode : public Page {
+   public:
     // PID of next child
-    uint64_t next_leaf_;
+    PID next_leaf_;
 
     // PID of previous child
-    uint64_t prev_leaf_;
+    PID prev_leaf_;
 
-    // Pointers to all data stored in this leaf
-    std::vector<ItemPointer> data_items_;
+    // Keys and pointers to all data stored in this leaf
+    std::vector<std::pair<KeyType, ItemPointer> > data_items_;
+
+    // Temporary node link used during structure modifications
+    PID side_link_;
+
+    // Min key in this node
+    KeyType low_key_;
+
+    // Max key in this node
+    KeyType high_key_;
+
+    LeafNode(): Page(LEAF_NODE), side_link_(NullPID), next_leaf_(NullPID), prev_leaf_(NullPID) {}
+  };
+
+  class SplitDelta : public Page {
+   public:
+    // Invalidate all records within the node greater than separator_
+    KeyType separator_;
+
+    PID side_link_;
+
+    SplitDelta(KeyType separator_key, PID new_sibling)
+        : Page(SPLIT_DELTA),
+          separator_(separator_key),
+          side_link_(new_sibling) {}
+  };
+
+  // Direct to records greater than low_separator_ and less than or equal to
+  // high_separator_
+  class IndexTermDelta : public Page {
+   public:
+    KeyType low_separator_;
+    KeyType high_separator_;
+
+    PID side_link_;
+
+    IndexTermDelta(KeyType low_separator_key, KeyType high_separator_key,
+                   PID new_sibling)
+        : Page(INDEX_TERM_DELTA),
+          low_separator_(low_separator_key),
+          high_separator_(high_separator_key),
+          side_link_(new_sibling) {}
+  };
+
+  // Stops all further use of node
+  class RemoveNodeDelta : public Page {
+   public:
+    RemoveNodeDelta() : Page(REMOVE_NODE_DELTA) {}
+  };
+
+  class NodeMergeDelta : public Page {
+   public:
+    // Direct all records greater than separator_ to physical_link_
+    KeyType separator_;
+
+    void* physical_link_;
+
+    NodeMergeDelta(KeyType separator_key, void* new_sibling)
+        : Page(NODE_MERGE_DELTA),
+          separator_(separator_key),
+          physical_link_(new_sibling) {}
+  };
+
+  class InsertDelta : public Page {
+   public:
+    KeyType key_;
+
+    ItemPointer location_;
+
+    InsertDelta(KeyType key, ItemPointer location)
+        : Page(INSERT_DELTA), key_(key), location_(location) {}
+  };
+
+  class DeleteDelta : public Page {
+   public:
+    KeyType key_;
+
+    ItemPointer location_;
+
+    DeleteDelta(KeyType key, ItemPointer location)
+        : Page(DELETE_DELTA), key_(key), location_(location) {}
   };
 
   // PID of the root node
-  uint64_t root_;
+  PID root_;
 
   // Maps node PIDs to memory locations
-  std::unordered_map<uint64_t, void*> map_table_;
-
+  std::unordered_map<PID, void*> map_table_;
 };
 
 }  // End index namespace
